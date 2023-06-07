be7071ff9798f79959979e2916cfa1371d677b7d
Yamisa11 <c.ya@live.com>
2023-06-07 14:59:55 +0200

    Added tests

--- Factory-function/med-scan.factory.js
+++ Factory-function/med-scan.factory.js
@@ -4,14 +4,14 @@ function MedScan() {
     var allergen = ''
     var side_effect = ''
     var precaution = ''
-    var nameprod = ''
+    var product_name = ''
 
     var pharma_products = [
         { image: "url", names: 'Alcophyllex', usages: 'a', side_effects: 'al', allergens: '', precautions: '1' },
         { image: "url", names: 'Flustat', usages: 'b', side_effects: 'fl', allergens: '', precautions: '2' },
         { image: "url", names: 'Soflax', usages: 'c', side_effects: 'so', allergens: '', precautions: '3' },
         { image: "url", names: 'Allergex', usages: 'd', side_effects: 'all', allergens: '', precautions: '4' },
-        { image: "url", names: 'Medazine', usages: 'e', side_effects: 'me', allergens: '', precautions: '5' },
+        { image: "url", names: 'Medazine', usages: 'e', side_effects: 'me', allergens: 'rash', precautions: '5' },
         { image: "url", names: 'Gastropect', usages: 'f', side_effects: 'ga', allergens: '', precautions: '6' }
     ]
 
@@ -22,8 +22,8 @@ function MedScan() {
     }
 
     function getPrecautions() {
-        for (var i = 0; pharma_products.length < 6; i++) {
-            if (pharma_products[i].names == nameprod) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == product_name) {
                 for (const y in pharma_products[i]) {
                     precaution = pharma_products[i].precautions
                 }
@@ -33,8 +33,8 @@ function MedScan() {
     }
 
     function getAllergens() {
-        for (var i = 0; pharma_products.length < 6; i++) {
-            if (pharma_products[i].names == nameprod) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == product_name) {
                 for (const y in pharma_products[i]) {
                     allergen = pharma_products[i].allergens
 
@@ -45,8 +45,8 @@ function MedScan() {
     }
 
     function getSideEffects() {
-        for (var i = 0; pharma_products.length < 6; i++) {
-            if (pharma_products[i].names == nameprod) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == product_name) {
                 for (const y in pharma_products[i]) {
                     side_effect = pharma_products[i].side_effects
 
@@ -57,8 +57,8 @@ function MedScan() {
     }
 
     function getUsages() {
-        for (var i = 0; pharma_products.length < 6; i++) {
-            if (pharma_products[i].names == nameprod) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == product_name) {
                 for (const y in pharma_products[i]) {
                     usage = pharma_products[i].usages
 
@@ -69,8 +69,8 @@ function MedScan() {
     }
 
     function getImage() {
-        for (var i = 0; pharma_products.length < 6; i++) {
-            if (pharma_products[i].names == nameprod) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == product_name) {
                 for (const y in pharma_products[i]) {
                     images = pharma_products[i].image
 
@@ -79,9 +79,8 @@ function MedScan() {
             }
         }
     }
-    function getPrecautions() {
-        return precaution
-    }
+
+   
     
     function errorHandling() {
 
--- test/med-scan.tests.js
+++ test/med-scan.tests.js
@@ -1,8 +1,28 @@
-describe('hi ', function () {
-    it('h', function () {
+describe('MedScan App Test ', function () {
+    it('should be able to get precautions of of Soflax', function () {
         var medScan = MedScan()
         medScan.getNameOfProduct('Soflax')
 
         assert.deepEqual('3', medScan.getPrecautions())
     })
+
+    it('should be able to get uses of Allergex', function () {
+        var medScan = MedScan()
+        medScan.getNameOfProduct('Allergex')
+
+        assert.deepEqual('d', medScan.getUsages())
+    })
+    it('should be able to get side effects of Flustat', function () {
+        var medScan = MedScan()
+        medScan.getNameOfProduct('Allergex')
+
+        assert.deepEqual('all', medScan.getSideEffects())
+    })
+    it('should be able to get Allergens of Medazine', function () {
+        var medScan = MedScan()
+        medScan.getNameOfProduct('Medazine')
+
+        assert.deepEqual('rash', medScan.getAllergens())
+    })
+   
 })
