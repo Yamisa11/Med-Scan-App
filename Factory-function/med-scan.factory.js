9738e0c074cf4202264f9e193acfc8f9ed72e72d
Asisipho <missfente@gmail.com>
2023-06-07 12:09:16 +0200

    shasha

--- Factory-function/med-scan.factory.js
+++ Factory-function/med-scan.factory.js
@@ -4,54 +4,123 @@ function MedScan() {
     var allergen = ''
     var side_effect = ''
     var precaution = ''
-    var product_name = ''
 
 
     var pharma_products = [
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' },
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' },
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' },
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' },
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' },
-        { image: "url", names: 'Alcophyllex', usages: '', side_effects: '', allergens: '', precautions: '' }
+        { image: "url", names: 'Alcophyllex', usages: 'a', side_effects: 'dc', allergens: 'f', precautions: 'r' },
+        { image: "url", names: 'Alcophyllex', usages: 'c', side_effects: 'd', allergens: 'f', precautions: 'r' },
+        { image: "url", names: 'Soflax', usages: 'f', side_effects: 'f', allergens: 'fente', precautions: '3' },
+        { image: "url", names: 'Alcophyllex', usages: 'cd', side_effects: 'd', allergens: 'f', precautions: 't' },
+        { image: "url", names: 'Alcophyllex', usages: 'd', side_effects: 'd', allergens: 'f', precautions: 't' },
+        { image: "url", names: 'Alcophyllex', usages: 'd', side_effects: 'e', allergens: 'f', precautions: 't' }
     ]
-    function displayProduct(name_of_product) {
-        for (var i = 0; i < 6; i++) {
-            if (pharma_products.names[i] == name_of_product) {
-                images = pharma_products.image[i]
-                usage = pharma_products.usages[i]
-                side_effect = pharma_products.side_effects[i]
-                allergen = pharma_products.allergens[i]
-                precaution = pharma_products.precautions[i]
+
+
+    function x() {
+        for (var i = 0; i > pharma_products.length; i++) {
+            console.log(pharma_products.names)
+        }
+    }
+    function getPrecautions() {
+        return precaution
+    }
+
+    function getPrecautions(name_of_product) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == name_of_product) {
+                for (const y in pharma_products[i]) {
+                    // console.log(pharma_products[i]);
+                    precaution = pharma_products[i].precautions
+
+                }
+                return precaution
+            }
+        }
+    }
+
+    function getAllergens(name_of_product) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == name_of_product) {
+                for (const y in pharma_products[i]) {
+                    // console.log(pharma_products[i]);
+                    allergen = pharma_products[i].allergens
+
+                }
+                return allergen
+            }
+        }
+    }
+
+    function getSideEffects(name_of_product) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == name_of_product) {
+                for (const y in pharma_products[i]) {
+                    // console.log(pharma_products[i]);
+                    side_effect = pharma_products[i].side_effects
+
+                }
+                return side_effect
+            }
+        }
+    }
+
+    function getUsages(name_of_product) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == name_of_product) {
+                for (const y in pharma_products[i]) {
+                    // console.log(pharma_products[i]);
+                    usage = pharma_products[i].usages
+
+                }
+                return usage
+            }
+        }
+    }
+
+    function getImage(name_of_product) {
+        for (var i = 0; pharma_products.length < 7; i++) {
+            if (pharma_products[i].names == name_of_product) {
+                for (const y in pharma_products[i]) {
+                    // console.log(pharma_products[i]);
+                    images = pharma_products[i].image
+
+                }
+                return images
             }
         }
     }
 
 
-    function getImage() {
-        return images
-    }
+    //     images = pharma_products[i].image
+    //     usage = pharma_products[i].usages
+    //     side_effect = pharma_products[i].side_effects
+    //     allergen = pharma_products[i].allergens
+    //     precaution = pharma_products[i].precautions
 
-    function getUsages() {
-        return usage
-    }
 
-    function getSideEffects() {
-        return side_effect
-    }
-    function getAllergens() {
-        return allergen
-    }
 
-    function getPrecautions() {
-        return precaution
-    }
+    // function getImage() {
+    //     return images
+    // }
+
+    // function getUsages() {
+    //     return usage
+    // }
+
+    // function getSideEffects() {
+    //     return side_effect
+    // }
+    // function getAllergens() {
+    //     return allergen
+    // }
+
+
 
     function errorHandling() {
 
     }
 
     return {
-        getImage, getUsages, getSideEffects, getAllergens, displayProduct, errorHandling, getPrecautions
+        getImage, getUsages, getSideEffects, getAllergens, errorHandling, getPrecautions, x
     }
 }
---
+++ Tests/med-scan.tests.js
@@ -0,0 +1,7 @@
+describe('hi ', function () {
+    it('h', function () {
+        var medScan = MedScan()
+
+        assert.deepEqual('3', medScan.getAllergens('Soflax'))
+})
+})
