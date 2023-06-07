d619595d27b7d81b4e115838b4a3d345f94dc447
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-06-07 11:29:17 +0200

    created html index

---
+++ DOM/med-scan.dom.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
