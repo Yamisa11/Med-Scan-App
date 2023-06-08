74ffca132a2004060bffc9cbe574245879d450cd
Yamisa11 <majijayamisa@gmail.com>
2023-06-08 14:07:49 +0200

    changes

--- DOM/med-scan.dom.js
+++ DOM/med-scan.dom.js
@@ -1,85 +1,91 @@
-var displayPrecaution = document.querySelector(".displayPrecaution")
-var displayAllergens = document.querySelector(".Allergens")
-var displayEffects = document.querySelector('.effects')
-var displayUses = document.querySelector(".displayUses")
-var filterTopic = document.querySelector('.topics')
-var showBtn = document.querySelector('.show')
-var saveBtn = document.querySelector(".save")
+var displayPrecaution = document.querySelector(".displayPrecaution");
+var displayAllergens = document.querySelector(".Allergens");
+var displayEffects = document.querySelector(".effects");
+var displayUses = document.querySelector(".displayUses");
+var filterTopic = document.querySelector(".topics");
+var showBtn = document.querySelector(".show");
+var saveBtn = document.querySelector(".save");
+var container1 = document.querySelector('.container1')
+var container2 = document.querySelector('.container2')
+var container3 = document.querySelector('.container3')
 
+var medScan = MedScan();
 
-var medScan = MedScan()
-
-var Precautionvalue = ""
-var usesValue = ""
-var allergensValue = ""
-var effectsValue = ""
-var product = {}
-
+var Precautionvalue = "";
+var usesValue = "";
+var allergensValue = "";
+var effectsValue = "";
+var product = {};
 
 function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    // console.log(`Scan result: ${decodedText}`, decodedResult);
+  // Handle on success condition with the decoded text or result.
+  // console.log(`Scan result: ${decodedText}`, decodedResult);
 
+  medScan.getNameOfProduct(decodedText);
+  Precautionvalue = medScan.getPrecautions();
+  usesValue = medScan.getUsages();
+  allergensValue = medScan.getAllergens();
+  effectsValue = medScan.getSideEffects();
 
-medScan.getNameOfProduct(decodedText);
-Precautionvalue =medScan.getPrecautions()
-usesValue = medScan.getUsages()
-allergensValue = medScan.getAllergens()
-effectsValue = medScan.getSideEffects()
+  product = {
+    theName: decodedText,
+    precaut: Precautionvalue,
+    use: usesValue,
+    effect: effectsValue,
+    allergy: allergensValue,
+  };
 
-product = {
-    theName : decodedText,
-    precaut : Precautionvalue,
-    use : usesValue,
-    effect : effectsValue,
-    allergy : allergensValue
+  localStorage["Products"] = JSON.stringify(product);
+container1.classList.add('hidden')
+container2.classList.remove('hidden')
+container3.classList.add('hidden')
 }
 
-localStorage["Products"] = JSON.stringify(product)
-
-
-
-}
-
-var html5QrcodeScanner =  new Html5QrcodeScanner(
-	"reader", { fps: 10, qrbox: 250 });
+var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+  fps: 10,
+  qrbox: 250,
+});
 
 html5QrcodeScanner.render(onScanSuccess);
-var data = JSON.parse(localStorage.getItem("Products"))
+var data = JSON.parse(localStorage.getItem("Products"));
 console.log(data);
 
+window.addEventListener("DOMContentLoaded", (event) => {
+    var data = JSON.parse(localStorage.getItem("Products"));
+console.log(data);
+displayPrecaution.innerHTML = data.precaut;
+displayAllergens.innerHTML = data.allergy;
+displayEffects.innerHTML = data.effect;
+displayUses.innerHTML = data.use;
+});
 
-displayPrecaution.innerHTML = data.precaut
-displayAllergens.innerHTML = data.allergy
-displayEffects.innerHTML = data.effect
-displayUses.innerHTML = data.use
-    
-showBtn.addEventListener('click', function(){
-    if (filterTopic.value == "Effects") {
-        displayPrecaution.innerHTML = ""
-displayAllergens.innerHTML = ""
-displayEffects.innerHTML = data.effect
-displayUses.innerHTML = ""
-    }
-    if (filterTopic.value == "Uses") {
-        displayPrecaution.innerHTML = ""
-displayAllergens.innerHTML = ""
-displayEffects.innerHTML = ""
-displayUses.innerHTML = data.use
-    }
-    if (filterTopic.value == "Aller") {
-        displayPrecaution.innerHTML = ""
-displayAllergens.innerHTML = data.allergy
-displayEffects.innerHTML = ""
-displayUses.innerHTML = ""
-    }
-    if (filterTopic.value == "Prec") {
-        displayPrecaution.innerHTML = data.precaut
-displayAllergens.innerHTML = ""
-displayEffects.innerHTML = ""
-displayUses.innerHTML = ""
-    }
-})
+
+showBtn.addEventListener("click", function () {
+  if (filterTopic.value == "Effects") {
+    displayPrecaution.innerHTML = "";
+    displayAllergens.innerHTML = "";
+    displayEffects.innerHTML = data.effect;
+    displayUses.innerHTML = "";
+  }
+  if (filterTopic.value == "Uses") {
+    displayPrecaution.innerHTML = "";
+    displayAllergens.innerHTML = "";
+    displayEffects.innerHTML = "";
+    displayUses.innerHTML = data.use;
+  }
+  if (filterTopic.value == "Aller") {
+    displayPrecaution.innerHTML = "";
+    displayAllergens.innerHTML = data.allergy;
+    displayEffects.innerHTML = "";
+    displayUses.innerHTML = "";
+  }
+  if (filterTopic.value == "Prec") {
+    displayPrecaution.innerHTML = data.precaut;
+    displayAllergens.innerHTML = "";
+    displayEffects.innerHTML = "";
+    displayUses.innerHTML = "";
+  }
+});
 
 // saveBtn.addEventListener("click", function(){
 //     var productList = []
