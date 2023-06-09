2a51fa095ef31162fc0b47965aa122cc75f5bd73
Yamisa11 <majijayamisa@gmail.com>
2023-06-09 12:28:39 +0200

    gh-pages

--- DOM/med-scan.dom.js
+++ DOM/med-scan.dom.js
@@ -5,21 +5,21 @@ var displayUses = document.querySelector(".displayUses");
 var filterTopic = document.querySelector(".topics");
 var scan_again = document.querySelector(".scanagain");
 var showAll = document.querySelector(".all");
-var saveBtn = document.querySelector('.save')
+var saveBtn = document.querySelector(".save");
 var dropDownSelected = document.querySelector("select");
 var container1 = document.querySelector(".container1");
 var container2 = document.querySelector(".container2");
 var container3 = document.querySelector(".container3");
 var picture = document.querySelector(".image");
 let divElm = document.querySelector(".content");
-var viewAllBtn = document.querySelector('.viewAll')
+var viewAllBtn = document.querySelector(".viewAll");
 let allContent = document.querySelector(".allContent");
 let allImage = document.querySelector(".allImage");
 let allView = document.querySelector(".allView");
-let l1 = document.querySelector('.l1')
-let l2 = document.querySelector('.l2')
-let l3 = document.querySelector('.l3')
-let l4 = document.querySelector('.l4')
+let l1 = document.querySelector(".l1");
+let l2 = document.querySelector(".l2");
+let l3 = document.querySelector(".l3");
+let l4 = document.querySelector(".l4");
 
 var pic_img = "";
 var Precautionvalue = "";
@@ -27,7 +27,7 @@ var usesValue = "";
 var allergensValue = "";
 var effectsValue = "";
 var product = {};
-var productList = []
+var productList = [];
 
 // var data = JSON.parse(localStorage.getItem("Products")) || [];
 // console.log(data.theName);
@@ -52,6 +52,8 @@ function onScanSuccess(decodedText, decodedResult) {
     allergy: allergensValue,
   };
 
+  html5QrcodeScanner.pause();
+
   localStorage.setItem("Products", JSON.stringify(product));
   container1.classList.add("hidden");
   container2.classList.remove("hidden");
@@ -66,6 +68,8 @@ function onScanSuccess(decodedText, decodedResult) {
   displayEffects.innerHTML = data.effect;
   displayPrecaution.innerHTML = data.precaut;
   displayAllergens.innerHTML = data.allergy;
+
+ 
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
@@ -87,7 +91,7 @@ html5QrcodeScanner.render(onScanSuccess);
 // });
 
 dropDownSelected.addEventListener("change", (event) => {
-    var data = JSON.parse(localStorage.getItem("Products"));
+  var data = JSON.parse(localStorage.getItem("Products"));
   var { value } = event.target.options[event.target.selectedIndex];
 
   if (filterTopic.value == "all") {
@@ -95,101 +99,94 @@ dropDownSelected.addEventListener("change", (event) => {
     displayAllergens.innerHTML = data.allergy;
     displayEffects.innerHTML = data.effect;
     displayUses.innerHTML = data.use;
-    l1.innerHTML = "Uses"
-    l3.innerHTML = "Precaution"
-    l4.innerHTML = "Allergens"
-    l2.innerHTML = "Side Effects: "
-
+    l1.innerHTML = "Uses";
+    l3.innerHTML = "Precaution";
+    l4.innerHTML = "Allergens";
+    l2.innerHTML = "Side Effects: ";
   }
   if (filterTopic.value == "Effects") {
     displayPrecaution.innerHTML = "";
     displayAllergens.innerHTML = "";
     displayEffects.innerHTML = data.effect;
     displayUses.innerHTML = "";
-    l1.innerHTML = ""
-    l3.innerHTML = ""
-    l4.innerHTML = ""
-    l2.innerHTML = "Side Effects: "
+    l1.innerHTML = "";
+    l3.innerHTML = "";
+    l4.innerHTML = "";
+    l2.innerHTML = "Side Effects: ";
   }
   if (filterTopic.value == "Uses") {
     displayPrecaution.innerHTML = "";
     displayAllergens.innerHTML = "";
     displayEffects.innerHTML = "";
     displayUses.innerHTML = data.use;
-    l2.innerHTML = ""
-    l3.innerHTML = ""
-    l4.innerHTML = ""
-    l1.innerHTML = "Uses: "
+    l2.innerHTML = "";
+    l3.innerHTML = "";
+    l4.innerHTML = "";
+    l1.innerHTML = "Uses: ";
   }
   if (filterTopic.value == "Aller") {
     displayPrecaution.innerHTML = "";
     displayAllergens.innerHTML = data.allergy;
     displayEffects.innerHTML = "";
     displayUses.innerHTML = "";
-    l2.innerHTML = ""
-    l3.innerHTML = ""
-    l1.innerHTML = ""
-    l4.innerHTML = "Allergens: "
-
+    l2.innerHTML = "";
+    l3.innerHTML = "";
+    l1.innerHTML = "";
+    l4.innerHTML = "Allergens: ";
   }
   if (filterTopic.value == "Prec") {
     displayPrecaution.innerHTML = data.precaut;
     displayAllergens.innerHTML = "";
     displayEffects.innerHTML = "";
     displayUses.innerHTML = "";
-    l2.innerHTML = ""
-    l1.innerHTML = ""
-    l4.innerHTML = ""
-    l3.innerHTML = "Precaution: "
+    l2.innerHTML = "";
+    l1.innerHTML = "";
+    l4.innerHTML = "";
+    l3.innerHTML = "Precaution: ";
   }
 });
 
-
-
-
-scan_again.addEventListener("click", function(){
+scan_again.addEventListener("click", function () {
   container1.classList.remove("hidden");
   container2.classList.add("hidden");
+});
 
-})
-
-saveBtn.addEventListener("click", function (){
+saveBtn.addEventListener("click", function () {
   var data = JSON.parse(localStorage.getItem("Products"));
-  productList.push(data)
-  localStorage.setItem("savedList", JSON.stringify(productList))
-})
+  productList.push(data);
+  localStorage.setItem("savedList", JSON.stringify(productList));
+});
 
-viewAllBtn.addEventListener('click', function (){
+viewAllBtn.addEventListener("click", function () {
   container1.classList.add("hidden");
   container2.classList.add("hidden");
   container3.classList.remove("hidden");
   var data = JSON.parse(localStorage.getItem("savedList"));
 
   for (let i = 0; i < data.length; i++) {
+    console.log(data[i].image);
     var imagDiv = document.createElement("img");
     imagDiv.src = data[i].image;
     allContent.appendChild(imagDiv);
     var usesDiv = document.createElement("div");
-    allUses = document.createTextNode("Uses     :"+data[i].use);
+    allUses = document.createTextNode("Uses     :" + data[i].use);
     usesDiv.appendChild(allUses);
-    usesDiv.style.margin = '1em'
+    usesDiv.style.margin = "1em";
     allContent.appendChild(usesDiv);
     var effectsDiv = document.createElement("div");
-    allEffects = document.createTextNode("Side Effects     :"+data[i].effect);
+    allEffects = document.createTextNode("Side Effects     :" + data[i].effect);
     effectsDiv.appendChild(allEffects);
-    effectsDiv.style.margin = '1em'
+    effectsDiv.style.margin = "1em";
     allContent.appendChild(effectsDiv);
     var precauDiv = document.createElement("div");
-    allPrecau = document.createTextNode("Precautions     :"+data[i].precaut);
+    allPrecau = document.createTextNode("Precautions     :" + data[i].precaut);
     precauDiv.appendChild(allPrecau);
-    precauDiv.style.margin = '1em'
+    precauDiv.style.margin = "1em";
     allContent.appendChild(precauDiv);
     var allerDiv = document.createElement("div");
-    allAller = document.createTextNode("Allergens     :"+data[i].allergy);
+    allAller = document.createTextNode("Allergens     :" + data[i].allergy);
     allerDiv.appendChild(allAller);
-    allerDiv.style.margin = '1em'
+    allerDiv.style.margin = "1em";
     allContent.appendChild(allerDiv);
-    
   }
-})
-
+});
